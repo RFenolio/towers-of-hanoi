81ca16f68cfdedf04352e529b748ab936edef6b4
rfenolio <mordoc@gmail.com>
2015-08-11 22:21:23 -0700

    Solve Algorithm

--- index.js
+++ index.js
@@ -4,13 +4,43 @@ var config = require("./config");
 var move = actions.move.bind(actions);
 
 function solve(n){
-  // fill in solution here
-
-
-
-
-
+  moveStack(n, 0, 2);
 }
+
+var moveStack = function (size, start, end) {
+  //base
+  if(size === 1) {
+    move(start,end);
+  }
+  else {
+    var other = findOther(start, end);
+    moveStack(size - 1, start, other);
+    move(start, end);
+    moveStack(size - 1, other, end);
+  }
+}
+
+var findOther = function (start, end) {
+  if(start === 0 && end === 1) {
+    return 2;
+  }
+  else if (start === 0 && end === 2) {
+    return 1;
+  }
+  else if (start === 1 && end === 0) {
+    return 2;
+  }
+  else if (start === 1 && end === 2) {
+    return 0;
+  }
+  else if (start === 2 && end === 0) {
+    return 1;
+  }
+  else if (start === 2 && end === 1) {
+    return 0;
+  }
+}
+
 
 solve(config.pieces);
 actions.finish();
